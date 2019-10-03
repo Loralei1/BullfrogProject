a4d6f5aca01f67afecde46d6b479e756ca911dce
Meaghan Searle <msearle88@gmail.com>
2019-10-03 10:00:58 -0600

    start of trying to get script to work

---
+++ BullfrogAssembly/Assets/ColorChange.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+   
+
+    public Material[] materials;
+    public float duration; 
+
+    public Renderer rend;
+    
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        
+    }
+
+    private void Update()
+    {
+       
+
+        if (Input.GetKeyDown( "Space"))
+        {
+            float lerp = Mathf.PingPong(Time.time, duration) / duration;
+            rend.material.Lerp(materials[1], materials[0], lerp); 
+
+        }
+        
+    }
+}
